0f234e0973773042172cdd4bb8465f1affb1977c
najim1110c <62584572+najim1110c@users.noreply.github.com>
2024-10-05 10:44:42 +0600

    Initial commit

--- hello.py
+++ hello.py
@@ -1,4 +1,10 @@
-
+def factorial_recursive(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial_recursive(n - 1)
+        
+        
 def factorial_iterative(n):
     result = 1
     for i in range(1, n + 1):
